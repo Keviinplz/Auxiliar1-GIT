e5c7354e7570297ef99f2814fda585b5d14f2664
Kevin Pinochet Hernández <keviinplz@DESKTOP-T6BFMME.localdomain>
2020-09-09 19:00:28 -0300

    changing the subtitle of the page

---
+++ aux/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/keviinplz/universidad/ingsoft/aux1/Auxiliar1-GIT/aux/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
